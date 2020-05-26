527bff5c8df196a25d7f15dc884c4a1538dc20da
Yashbeer Singh <yashbeera@gmail.com>
2020-05-26 16:25:13 +0000

    prod fix

--- django-album/settings.py
+++ django-album/settings.py
@@ -144,10 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-#STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    #os.path.join(BASE_DIR, 'static')
 ]
 
 AWS_ACCESS_KEY_ID = get_env('AWS_ACCESS_KEY_ID', '')
